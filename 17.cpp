2d38cd8adfcb2a9d9797261e346507770e7bf71e
unknown <cocfanlike@gmail.com>
2025-06-28 20:24:24 +0545

    used typedef for using pair(1, pair(1,2)) of card name, suit and its value

--- 17.cpp
+++ 17.cpp
@@ -4,40 +4,43 @@
 #include <random>     // for std::default_random_engine
 #include <ctime> 
 
+
+typedef std::vector<std::pair<std::string, std::pair<std::string, int>>> Taas;
+
 class Cards{
     private:
         std::string SUIT[4] = { "HEART", "CLUB", "DIAMOND", "SPADE" }; 
         std::string VALUE[13] = { "ONE", "TWO", "THREE", "FOUR", "FIVE", "SIX", "SEVEN", "EIGHT", "NINE", "TEN", "JACK", "QUEEN", "KING"};
-
+        int Number[13] ={1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
     public:
-        std::vector<std::pair<std::string, std::string>> deck(){
-            std::vector<std::pair<std::string, std::string>> Deck;
+        Taas deck(){
+            Taas Deck;
             for (int i=0; i < 4; i++){
                 for (int j=0; j < 13; j++)
                 {
                     
-                    Deck.push_back({VALUE[j], SUIT[i]});
+                    Deck.push_back({VALUE[j], {SUIT[i], Number[j]}});
                 }
 
             }
 
             for (auto cards : Deck)
             {
-                std::cout << cards.first <<" of "<< cards.second<<std::endl;
+                std::cout << cards.first <<" of "<< cards.second.first<<" of value "<<cards.second.second<<std::endl;
             }
             
             return Deck;
 
         }
 
-        std::vector<std::pair<std::string, std::string>> shuffle(std::vector<std::pair<std::string, std::string>> Deck){
+        Taas shuffle(Taas Deck){
             std::default_random_engine rng(static_cast<unsigned>(std::time(nullptr)));
             std::cout <<rng<<std::endl;
             std::shuffle(Deck.begin(), Deck.end(), rng);
 
             for (auto cards : Deck)
             {
-                std::cout << cards.first <<" of "<< cards.second<<std::endl;
+                std::cout << cards.first <<" of "<< cards.second.first<<" of value "<<cards.second.second<<std::endl;
             }
 
 
@@ -49,7 +52,7 @@ class Cards{
 int main() {
 
     Cards card1;
-    std::vector<std::pair<std::string, std::string>> myDeck = card1.deck();
+    Taas myDeck = card1.deck();
     card1.shuffle(myDeck);
 
 
